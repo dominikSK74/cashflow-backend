6093debfe82d299a8ae9e82b85311c886c6f4613
Dominik Skąpski <dominiksk47@gmail.com>
2022-11-16 11:00:12 +0100

    Project created

---
+++ src/main/java/pl/sci/cashflowbackend/CashflowBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.sci.cashflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashflowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashflowBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sci/cashflowbackend/CashflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sci.cashflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashflowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
